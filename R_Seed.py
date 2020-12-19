b5782ae34186d16e3c5e314d2844a567c58594e0
Rashi Anubhi Srivastava <rashisrivastava2001@gmail.com>
2020-12-20 01:53:56 +0530

    Add files via upload

---
+++ R_Seed.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 12 19:21:01 2020
+
+@author: rashi
+"""
+
+import json
+from flask import Flask,request,jsonify,Response, render_template, url_for
+import base64
+import numpy as np
+import imageio
+from matplotlib.pyplot import imshow
+from keras.preprocessing import image
+from types import SimpleNamespace 
+import pandas as pd
+import joblib
+import requests
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/Seeds', methods = ["GET"])
+def products():
+    #data = request.get_json(force = "true")
+    with open("Seed.json") as f:
+        file = json.load(f)
+    
+    
+    return (file)
+
+if __name__ == "__main__":
+    app.run()
+    
